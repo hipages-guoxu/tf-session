e994936abd4ca55bbbfd6333c936e65a1b5ac06a
hiluluke <hiluluke@hotmail.com>
2022-04-01 14:51:23 +0800

    add module

--- main.tf
+++ main.tf
@@ -2,32 +2,8 @@ provider "aws" {
   region = "ap-southeast-2"
 }
 
-resource "aws_security_group" "demo_instance_sg" {
-  name = "demo-instance-sg"
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "TCP"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "TCP"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_instance" "demo_instance" {
-  ami = var.ami_id
-  instance_type = var.instance_type
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
-              EOF
-  vpc_security_group_ids= [aws_security_group.demo_instance_sg.id]
-  tags = {
-    Name = "demo"
-  }
+module "demo-web" {
+  source = "./modules/web-module"
+  instance_type = "t2.micro"
+  ami_id = "ami-0b7dcd6e6fd797935"
 }
---
+++ modules/web-module/main.tf
@@ -0,0 +1,29 @@
+resource "aws_security_group" "demo_instance_sg" {
+  name = "demo-instance-sg"
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "demo_instance" {
+  ami = var.ami_id
+  instance_type = var.instance_type
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+  vpc_security_group_ids= [aws_security_group.demo_instance_sg.id]
+  tags = {
+    Name = "demo"
+  }
+}
